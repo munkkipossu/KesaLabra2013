cb22cb49b949c74cfa2210fb75ce60eedb6938f3
Teemu O Laakso <tolaakso@svm-27.cs.helsinki.fi>
2013-08-22 22:43:17 +0300

    Muutama sekvenssikaavio, luokkien peruskoirjailua, toimintalogiikan perustuksia

---
+++ Cuarenta/src/TheGame/Table.java
@@ -0,0 +1,39 @@
+
+package TheGame;
+
+/**
+ * Table is a 'CardHolder' that knows which card was last played.
+ * For now, this is enough to make it it's own class, but we'll see
+ * if I come up with more things for it to do.
+ * 
+ * @author tolaakso
+ */
+public class Table extends CardHolder{
+    
+    private Card lastCardPlayed;
+    
+    public Table(){
+        super();
+        this.lastCardPlayed = null;
+    }
+
+    public Card getLastCardPlayed(){
+        return this.lastCardPlayed;
+    }
+    
+    public void addCard(Card card){
+        this.lastCardPlayed = card;
+        super.addCard(card);
+    }
+    
+    public Card takeCard(int whichCard){
+        Card card = super.removeCard(whichCard);
+        
+        if(this.lastCardPlayed.equals(card)){
+            this.lastCardPlayed = null;
+        }
+        return card;
+    }
+    
+    
+}
