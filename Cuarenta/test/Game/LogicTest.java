3c5c4a2c74eadafe65ebc5abd3832044ad5eca24
Teemu Laakso <teemu.o.laakso@helsinki.fi>
2013-09-12 23:51:33 +0300

    Kirjoitin muutaman Junitestin lisaa

--- Cuarenta/test/Game/LogicTest.java
+++ Cuarenta/test/Game/LogicTest.java
@@ -4,9 +4,6 @@
  */
 package Game;
 
-import Game.Rank;
-import Game.Card;
-import Game.Suit;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -14,45 +11,84 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import Game.*;
+import java.util.ArrayList;
+
 /**
  *
- * @author tolaakso
+ * @author mp
  */
 public class LogicTest {
     
     public LogicTest() {
     }
-
+    
     @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
+    public static void setUpClass() {
 
+    }
+    
     @AfterClass
-    public static void tearDownClass() throws Exception {
+    public static void tearDownClass() {
     }
     
     @Before
     public void setUp() {
+        game = new CuarentaGame("human", true);
     }
     
     @After
     public void tearDown() {
     }
- 
-    
+
     @Test
-    public void caidaSameCards(){
-        Card card1 = new Card(Rank.ACE, Suit.HEARTS);
-        Card card2 = new Card(Rank.ACE, Suit.DIAMONDS);
+    public void pointsAddedIfLastCardPlayedIsChosen() { 
+        Card tableCard = new Card(Rank.QUEEN, Suit.HEARTS);
+        game.getTable().addCard(tableCard);
+        ArrayList<Card> chosenCards = new ArrayList<Card>();
+        chosenCards.add(tableCard);        
         
-        asserTrue(caidaCheck(card1, card2));
+        Card playedCard = new Card(Rank.QUEEN, Suit.CLUBS);
+        game.getHuman().addCard(playedCard);
+        
+        humanMove(game, playedCard, chosenCards);
+        
+        assertTrue(game.getHumanPlayer.getPoints() == 2);
+    }
+
+    @Test
+    public void pointsAddedIfTableEmptied() { 
+        
+    }
+
+    @Test
+    public void chosenCardPlacedToDicardpileIfWrongChoice() { 
+        
+    }
+
+    @Test
+    public void chosenCardPlacedToDicardpileIfWrongChoice() { 
+        
+    }
+
+    @Test
+    public void zeroCardsChosenPlayedCardAddedToDiscardpile() { 
+        
     }
     
-    
     @Test
-    public void (){
+    public void oneWrongCardChosenPlayedCardAddedToDiscardpile() { 
         
-    }       
-            
+    }
+
+    @Test
+    public void moreThanOneWrongCardChosenPlayedCardAddedToDiscardpile() { 
+        
+    }
+
+    @Test
+    public void cardAdditionWorks() { 
+        
+    }
 
 }
